e0956be99f2654c7c3cb54bc4626b5ccf47b85b7
ivan kurkin <ivan.kurkin@softgamings.com>
2022-02-16 21:40:51 +0300

    create component

--- App.tsx
+++ App.tsx
@@ -1,25 +1,29 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import main from "configs";
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import TextComponent from './src/TextComponent';
+import main from 'configs';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
-      <Text>{main.name}</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <TextComponent />
+            <Text style={styles.text}>
+                Open up App.tsx to start working on your app!
+            </Text>
+            <Text>{main.name}</Text>
+            <StatusBar style='auto' />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "#b70e0e",
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: '#b70e0e',
+    },
 });
